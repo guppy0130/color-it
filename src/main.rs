57499548c59bca1c690ffd806caf37c6377bd343
guppy0130 <guppy0130@yahoo.com>
2024-02-15 23:00:02 -0600

    control the amount of color
    *  or  for more rainbow

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, FixedOffset};
-use clap::Parser;
+use clap::{Parser, ArgAction::Count};
 use log::{logger, Level, RecordBuilder};
 use owo_colors::{OwoColorize, Stream::Stdout, Style};
 use std::{
@@ -11,20 +11,27 @@ use std::{
 #[derive(Parser, Debug)]
 #[command(version, about)]
 struct Config {
+    /// json key to read for for level/severity
     #[arg(short, long, default_value = "level")]
     level: String,
+    /// json key to read for log message
     #[arg(short, long, default_value = "message")]
     message: String,
+    /// json key to read for log timestamp
     #[arg(short, long, default_value = "timestamp")]
     timestamp: String,
+    /// how to parse timestamp (defaults to ISO8601)
     #[arg(short, long, default_value = "%+")]
     strptime: String,
+    /// how much color to use (0..2, 0 is just level)
+    #[arg(short, long, action=Count)]
+    color_amount: u8
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let config = Config::parse();
     env_logger::builder()
-        .format(|buf, record| {
+        .format(move |buf, record| {
             let kv = record.key_values();
             let ts_str: &str = &kv.get("timestamp".into()).unwrap().to_string();
             // defaulting to ISO8601 because we're sane and we don't want to just guess
@@ -44,14 +51,25 @@ fn main() -> Result<(), Box<dyn Error>> {
             writeln!(
                 buf,
                 "{} {: <6} - {}",
-                // wait, how much do I color?
-                dt.to_rfc3339(),
+                dt.to_rfc3339().if_supports_color(Stdout, |text| {
+                    if config.color_amount >= 2 {
+                        text.style(style)
+                    } else {
+                        text.style(Style::new())
+                    }
+                }),
                 record
                     .level()
                     .if_supports_color(Stdout, |text| text.style(style)),
                 record
                     .args()
-                    .if_supports_color(Stdout, |text| text.style(style))
+                    .if_supports_color(Stdout, |text| {
+                        if config.color_amount >= 1 {
+                            text.style(style)
+                        } else {
+                            text.style(Style::new())
+                        }
+                    })
             )
         })
         .init();
